af9ad18c813b22960e2b930ce5b6e4a9dc3163de
闫伟 <yanwei@yanweideMacBook-Pro.local>
2017-05-17 16:45:15 +0800

    error fix

--- src/org/bigraph/bigsim/model/BiNode.scala
+++ src/org/bigraph/bigsim/model/BiNode.scala
@@ -12,11 +12,7 @@ object BiNode{
   
 }
 
-<<<<<<< HEAD
-class BiNode(b: Bigraph, childPair: Map[Int, Int]) {
-  var bigraph: Bigraph = b;
-  var childPair: Map[Int, Int] = null
-=======
+
 class TSPair{
   var T: ReactionRule = null; //trans
   var S: BiNode = null;  //state
@@ -26,7 +22,7 @@ class TSPair{
 
 class BiNode(b: Bigraph, child: TSPair) {
   var bigraph: Bigraph = b;
->>>>>>> 207ff948d72d13445ec5c8a31e93c6dc86abf4ed
+
   var hash: Int = {//根据当前偶图的root生成唯一的hashCode
     if (bigraph.root != null)
       bigraph.root.toString.hashCode();
@@ -36,13 +32,5 @@ class BiNode(b: Bigraph, child: TSPair) {
   def GetChild: List[TSPair] = {
     return null;
   }
-<<<<<<< HEAD
-=======
-}
 
-class TSPair{
-  var T: ReactionRule = null; //trans
-  var S: BiNode = null;  //state
-  var pid: String = ""; //this trans belongs which process
->>>>>>> 2d1ee42e7a27809a6d7e7cdccf52caca2390c0de
 }
--- src/org/bigraph/bigsim/model/TransitionSystem.scala
+++ src/org/bigraph/bigsim/model/TransitionSystem.scala
@@ -5,22 +5,7 @@ import scala.collection.mutable.Map
 /**
  * @author amy
  */
-<<<<<<< HEAD
 
-=======
-object TransitionSystem {
-  def GetHead: BiNode = {
-    var res: BiNode = null;
-    return res;
-  }
-  
-  
-  
-  def GetTransFromNode(parBiNode: BiNode, childBiNode: BiNode): ReactionRule = {
-    return null;
-  }
-}
->>>>>>> 207ff948d72d13445ec5c8a31e93c6dc86abf4ed
 
 class TransitionSystem(init: BiNode) {
   val root: BiNode = init;
